feb8cccd9379ed09ec6067af2921a20bcc0699a6
GekiRyuKeN <gamingaurav@gmail.com>
2024-07-14 12:22:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,8 +103,5 @@ elif source_radio == settings.VIDEO:
     helper.play_stored_video(confidence, model)
 
 
-elif source_radio == settings.YOUTUBE:
-    helper.play_youtube_video(confidence, model)
-
 else:
     st.error("Please select a valid source type!")
